9c1e8350f1a4244e5e74f39031c09bee43f28d6d
Melanie Seltzer <melleh11@gmail.com>
2018-12-04 15:27:00 -0800

    chore: Updates (#2)
    - Modified Eslint and Prettier config
    - Added Husky and lint-staged to the project
    - Updated dependencies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return <div>Hello world</div>;
-  }
-}
+const App = () => <div>Hello world!</div>;
 
 export default App;
